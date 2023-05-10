0c258ecf6ffc765088217e8917a8baad1054129c
jean-claude <jean-claude.grech@orange.fr>
2023-05-10 16:36:13 +0200

    first commit

---
+++ OneDrive/Bureau/atelier nivola/index.js
@@ -0,0 +1,11 @@
+const navE = document.querySelector('.nav');
+const hamburgerE = document.querySelector('.hamburger');
+const main = document.querySelector("main");
+
+hamburgerE.addEventListener("click", () => {
+  navE.classList.toggle('nav--open');
+
+
+
+  
+});
