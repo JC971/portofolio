6b222e5e030b4025d3a60a4f4ee48b07d2ad8796
jean-claude <jean-claude.grech@orange.fr>
2023-06-06 11:06:08 +0200

    rdd

--- OneDrive/Bureau/fishE/Front-End-Fisheye/scripts/pages/photographer.js
+++ OneDrive/Bureau/fishE/Front-End-Fisheye/scripts/pages/photographer.js
@@ -1,4 +1,4 @@
-
+/*
 function photographerFactory(data) {
   const { name, portrait, city, country, tagline, price, id } = data;
 
@@ -57,12 +57,72 @@ fetch('data/photographers.json')
 
     for (let i = 0; i < photographers.length; i++) {
       const photographerData = photographers[i];
+
+      
       
       let nom = document.getElementById('nom');
       nom.innerHTML = photographerData.name;
       
  
       
+    }
+  });*/
+
+
+function photographerFactory(data) {
+  const { name, portrait, city, country, tagline, price, id } = data;
+
+  const picture = `assets/photographers/${portrait}`;
+
+  function getUserCardDOM() {
+    // Code pour générer et retourner la carte utilisateur
+  }
+
+  return { name, picture, city, country, price, id, getUserCardDOM };
+}
+
+// Récupérer tous les liens
+const links = document.querySelectorAll('a');
+
+// Ajouter un gestionnaire d'événements à chaque lien
+links.forEach(link => {
+  link.addEventListener('click', event => {
+    // Empêcher le comportement par défaut du lien
+    event.preventDefault();
+
+    // Récupérer l'ID du photographe à partir de l'élément HTML
+    const photographerId = link.id;
+
+    // Construire la nouvelle URL avec l'ID du photographe en tant que paramètre de requête
+    const newUrl = `https://Front-End-Fisheye/photographer.html?id=${photographerId}`;
+
+    window.location.href = newUrl;
+  });
+});
+
+const url = window.location.href;
+console.log('L\'URL de la page est', url);
+let photographerId = url.split('=')[1];
+
+fetch('data/photographers.json')
+  .then(response => response.json())
+  .then(data => {
+    const photographers = data.photographers;
+
+    for (let i = 0; i < photographers.length; i++) {
+      const photographerData = photographers[i];
+      
+      // Vérifier si l'ID du photographe correspond à celui extrait de l'URL
+      if (photographerData.id === photographerId) {
+        // Afficher les détails du photographe
+        let nom = document.getElementById('nom');
+        nom.innerHTML = photographerData.name;
+
+        // Ajouter le reste du code pour afficher les autres détails du photographe
+        // ...
+
+        break; // Sortir de la boucle une fois que le photographe correspondant est trouvé
+      }
     }
   });
 
